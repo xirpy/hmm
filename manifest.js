d65b25ff8c006154e8f4d0c2fe4aa2d8fdc83b7d
Xirpy <39977285+xirpy@users.noreply.github.com>
2023-08-07 16:53:26 -0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Nome do Seu Site",
+  "short_name": "NomeCurto",
+  "description": "Uma breve descrição do seu PWA",
+  "icons": [
+    {
+      "src": "icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#FFFFFF",
+  "theme_color": "#000000"
+}
